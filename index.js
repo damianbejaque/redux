7606959944ca9757170d3ee4b1a306612b03c6bb
Damian Bejarano <damianbejaque@gmail.com>
2020-11-02 13:56:51 -0500

    create a new store with a single action called ADD_TODO

---
+++ index.js
@@ -0,0 +1,47 @@
+//Libraty Code
+function createStore(reducer){
+    let state
+    let listeners = []
+
+
+    const getState = () => state
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
+    }
+
+    return {
+        getState, 
+        subscribe, 
+        dispatch
+    }
+}
+
+//App Code
+
+function todos (state = [], action){
+    if(action.type === 'ADD_TODO'){
+        return state.concat([action.todo])
+    }
+    return state
+}
+
+const store = createStore(todos)
+store.subscribe(() => {
+    console.log('The new state is :', store.getState())
+})
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+})
